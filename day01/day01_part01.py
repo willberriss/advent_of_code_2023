b7acb7b81e504af0f0e6c14e88ba6d5211af9d77
Will Berriss <10340373+willberriss@users.noreply.github.com>
2024-06-29 13:59:35 +0100

    AoC 2023 Day 01 Part 01 solution.

---
+++ day01/day01_part01.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+def day01_part01(datafile):
+    sum = 0
+    for num in num_generator(datafile):
+        # print(f"num: {num}")
+        sum += num
+    
+    print(f"sum: {sum}")
+    return sum
+   
+def num_generator(datafile):
+    print("Opening file: %s" % datafile)
+    f = open(datafile, 'r')
+    
+    sum = 0.0
+    for row in open(datafile, 'r'):
+        # print(f"row: {row}")
+        num = get_num(row)
+        yield num
+    
+    print(f"digit_generator is now empty")
+    return
+
+# 
+# A row of data might have just 1 digit, or 2 or more. 
+# The row might contain 2 or more consecutive digits too, e.g. 34
+# We want exactly TWO digits, the FIRST and the LAST digit in the string, which could be the same
+# digit if there is only 1. If there is only 1 digit, that is the first and the last!
+#
+def get_num(row):
+    alldigits = re.findall("[0-9]", row)
+    d1 = alldigits[0]
+    d2 = alldigits[-1]
+    
+    # Alternatively we could unpack as follows:
+    # *x, last = alldigits # unpack - there will be at least 1 digit
+    # print(f"last: {last}")
+    
+    twodigits = d1 + d2
+    num = int(twodigits)
+    return num
+
+if __name__ == '__main__':
+    print("We've been called directly, not imported.")
+    datafile ="input.txt"
+    if len(sys.argv) > 1:
+        datafile = sys.argv[1]
+    print("datafile: %s" % datafile)
+    
+    the_sum = day01_part01(datafile)
+    #print(f"Answer = %d" % the_sum)
+    print(f"Answer = {the_sum}")
+    #print(f"Answer = {the_sum:.5f}")
+    
+
